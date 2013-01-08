74a024f429ef64b94261560bbf683fa878940d06
zouxu <zouivex@gmail.com>
2013-01-08 14:54:12 -0800

    None

--- go/capture.go
+++ go/capture.go
@@ -5,29 +5,35 @@ import "os"
 
 func main(){
 	if len(os.Args) != 2 {
-		fmt.Println("Usage:", os.Args[0], "Local IP")
-		return
-	}
- 
-	addr, err := net.ResolveIPAddr("ip", os.Args[1])
-	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Usage:", os.Args[0], "LocalIP")
 		return
 	}
 
-	conn, err := net.ListenIP("ip4:tcp", addr)
+	captureProtocol("ip4:tcp", os.Args[1])
+	// captureProtocol("ip4:icmp", os.Args[1])
+	// captureProtocol("ip4:udp", os.Args[1])
+}
+
+func captureProtocol(proto string, localAddr string){
+	conn, err := net.ListenPacket(proto, localAddr)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer conn.Close()
-	
+
 	buffer := make([]byte, 1024)
 	for {
 		n, _, err := conn.ReadFrom(buffer)
 		if err != nil {
 			fmt.Println(err)
+			return
 		}
-		fmt.Println(buffer[0:n])
+		handlerPacket(proto, buffer[0:n])
 	}
+}
+
+func handlerPacket(proto string, buffer []byte) {
+	fmt.Println(proto)
+	fmt.Println(buffer)
 }
