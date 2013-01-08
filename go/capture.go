868c5d7a42c36fdd9c6ed5d4ee72ba0676799fae
zouxu <zouivex@gmail.com>
2013-01-08 13:49:31 -0800

    Update capture.go

--- go/capture.go
+++ go/capture.go
@@ -1,21 +1,33 @@
 package main
 import "net"
 import "fmt"
+import "os"
 
 func main(){
-	conn, err := net.ListenPacket("ip4:icmp", "0.0.0.0")
+	if len(os.Args) != 2 {
+		fmt.Println("Usage:", os.Args[0], "Local IP")
+		return
+	}
+ 
+	addr, err := net.ResolveIPAddr("ip", os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	conn, err := net.ListenIP("ip4:tcp", addr)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer conn.Close()
 	
-	buffer := make([]byte, 256)
+	buffer := make([]byte, 1024)
 	for {
-		_, _, err := conn.ReadFrom(buffer)
+		n, _, err := conn.ReadFrom(buffer)
 		if err != nil {
 			fmt.Println(err)
 		}
-		fmt.Println(buffer)
+		fmt.Println(buffer[0:n])
 	}
 }
