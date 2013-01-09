e3386c56d927bd656376744d094b08606dd47bc8
zou xu <zouivex@gmail.com>
2013-01-09 21:32:09 +0800

    Implement ICMP protocol

--- go/capture.go
+++ go/capture.go
@@ -9,8 +9,8 @@ func main(){
 		return
 	}
 
-	captureProtocol("ip4:tcp", os.Args[1])
-	// captureProtocol("ip4:icmp", os.Args[1])
+	//captureProtocol("ip4:tcp", os.Args[1])
+	captureProtocol("ip4:icmp", os.Args[1])
 	// captureProtocol("ip4:udp", os.Args[1])
 }
 
@@ -34,6 +34,23 @@ func captureProtocol(proto string, localAddr string){
 }
 
 func handlerPacket(proto string, buffer []byte) {
-	fmt.Println(proto)
-	fmt.Println(buffer)
+	if proto == "ip4:icmp" {
+		parseICMP(buffer)
+	} else if proto == "ip4:tcp" {
+
+	} else if proto == "ip4:udp" {
+
+	} else {
+		fmt.Println("Unknown protocol")
+	}
+}
+
+func parseICMP(buffer []byte){
+	typeName := int(buffer[0])
+	code := int(buffer[1])
+	checkSum := int(buffer[2])<<8 | int(buffer[3])
+	id := int(buffer[4])<<8 | int(buffer[5])
+	seq := int(buffer[6])<<8 | int(buffer[7])
+
+	fmt.Printf("Type=%d, Code=%d, Check Sum=%d, ID=%d, Seq Number= %d\n", typeName, code, checkSum, id, seq)
 }
