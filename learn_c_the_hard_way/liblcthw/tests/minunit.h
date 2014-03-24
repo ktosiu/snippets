470f5cb17f5b0d37846aac9bdb0c1b92b11e0217
zouivex <qoo@g-mac-2.local>
2014-03-25 00:09:03 +0800

    Refact the unittest

--- learn_c_the_hard_way/liblcthw/tests/minunit.h
+++ learn_c_the_hard_way/liblcthw/tests/minunit.h
@@ -1,28 +1,33 @@
-#undef NDEBUG
 #pragma once
 
 #include <stdio.h>
-#include "lcthw/debug.h"
+#include <lcthw/debug.h>
 #include <stdlib.h>
-#define mu_suite_start() char *message = NULL;
 
-#define mu_assert(test, message) if(!(test)) { log_err(message); return message;}
-#define mu_run_test(test) debug("\n------%s", " " #test);\
+#define mu_suite_start() char *message = NULL;
+#define mu_assert(test, message)            \
+    if(!(test)) { log_err(message); return message;}
+#define mu_run_test(test)                   \
+    debug("\n------%s", " " #test);         \
     message = test(); tests_run++; if(message) return message;
 
-#define RUN_TESTS(name) int main(int argc, char *argv[]) {\
-    argc = 1; \
-    debug("\n------ RUNNING:%s", argv[0]);\
-        printf("------\nRUNNING:%s\n", argv[0]);\
-        char *result = name(); \
-        if(result != 0){\
-            printf("Failed: %s\n", result); \
-        }\
-        else {\
-            printf("All tests passed\n");\
-        }\
-    printf("Tests run: %d\n", tests_run); \
-    exit(result != 0);\
+#define RUN_TESTS(name)                     \
+int main(int argc, char *argv[]) {          \
+    if(argc != 1) {                         \
+        log_err("Argument error\n");        \
+        return 1;                           \
+    }                                       \
+    debug("\n------ RUNNING:%s", argv[0]);  \
+    printf("------\nRUNNING:%s\n", argv[0]);\
+    char *result = name();                  \
+    if(result != 0) {                       \
+       printf("Failed: %s\n", result);      \
+    }                                       \
+    else {                                  \
+        printf("All tests passed\n");       \
+    }                                       \
+    printf("Tests run: %d\n", tests_run);   \
+    return(result != NULL);                      \
 }
 
 int tests_run;
