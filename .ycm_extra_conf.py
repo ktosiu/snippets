72644a6eba328f0004d75524427356d6c90970fc
zouivex <zouivex@gmail.com>
2014-06-02 22:47:07 -0700

    Add stl path to the ycm_config

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -60,6 +60,7 @@ flags = [
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x','c++',
 '-I','.',
+'-isystem', '/usr/include/c++/4.8',
 '-isystem','/usr/include',
 '-isystem','/usr/local/include',
 ]
