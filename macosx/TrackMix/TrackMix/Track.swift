a42a553c593fa3fdb2a2433ae6b46b9964bd1c96
zou.xu <zouivex@gmail.com>
2014-06-28 17:18:43 +0800

    Add Trackmix example with swift lang

---
+++ macosx/TrackMix/TrackMix/Track.swift
@@ -0,0 +1,17 @@
+//
+//  Track.swift
+//  TrackMix
+//
+//  Created by zouivex on 2014/6/28.
+//  Copyright (c) 2014年 zouxu. All rights reserved.
+//
+
+import Cocoa
+
+class Track: NSObject {
+    var volume:CFloat
+    
+    init() {
+        volume = 0
+    }
+}
---
+++ macosx/TrackMix/TrackMix/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  TrackMix
+//
+//  Created by zouivex on 2014/6/28.
+//  Copyright (c) 2014年 zouxu. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
