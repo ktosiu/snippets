81eb0b78a89cf4212718dd2108b99bbb499081f0
zou.xu <zouivex@gmail.com>
2014-07-06 17:53:07 +0800

    Sample code to use delegate

---
+++ macosx/WindowDelegate/WindowDelegate/WDController.h
@@ -0,0 +1,13 @@
+//
+//  WDController.h
+//  WindowDelegate
+//
+//  Created by zouivex on 2014/7/6.
+//  Copyright (c) 2014年 zouxu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WDController : NSObject<NSWindowDelegate>
+
+@end
---
+++ macosx/WindowDelegate/WindowDelegate/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  WindowDelegate
+//
+//  Created by zouivex on 2014/7/6.
+//  Copyright (c) 2014年 zouxu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
