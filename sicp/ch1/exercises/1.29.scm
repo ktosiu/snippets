6b2b5cdfec5b708f756c8325173a371b87491df2
zou.xu <zouivex@gmail.com>
2014-08-20 21:42:28 +0800

    More exercises

--- sicp/ch1/exercises/1.29.scm
+++ sicp/ch1/exercises/1.29.scm
@@ -1,31 +1,19 @@
-(define (product term a next b)
+(define (sum term a next b)
   (if (> a b)
-      1
-      (* (term a)
-         (product term (next a) next b))))
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
 
-(define (product-iter term a next b)
-  (define (iter a result)
-    (if (> a b)
-        result
-        (iter (next a)
-              (* (term a) result))))
-  (iter a 1))
+(define (simpson-integral f a b n)
+  (let ((h (/ (- b a) n)))
+    (define (simpson-term k)
+      (* (f (+ a (* k h)))
+         (cond ((or (= 0 k) (= n k)) 1)
+               ((odd? k) 4)
+               (else 2))))
+    (* (/ h 3.0) (sum simpson-term 0 (lambda(x) (+ x 1)) n))))
 
-(define (product-integer a b)
-  (product-iter (lambda(x) x)
-           a
-           (lambda(x) (+ x 1))
-           b))
-(product-integer 1 5)
+(define (cube x) (* x x x))
 
-(define (square x) (* x x))
-
-(define (pi-product a b)
-  (product-iter
-     (lambda(n) (/ (* 2.0 2 n (+ n 1)) (square (+ (* 2 n) 1))))
-     a
-     (lambda(x) (+ x 1))
-     b))
-
-(* 4 (pi-product 1 10000))
+(simpson-integral sin 0 1 100)
+(simpson-integral cube 0 1 100)
---
+++ sicp/ch1/exercises/1.32.scm
@@ -0,0 +1,28 @@
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate combiner null-value term (next a) next b))))
+
+(define (accumulate-iter combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a)
+              (combiner (term a)
+                        result))))
+  (iter a null-value))
+
+(define (sum term a next b)
+  (accumulate-iter + 0 term a next b))
+
+(define (product term a next b)
+  (accumulate-iter * 1 term a next b))
+
+(sum
+ (lambda(x) x)
+ 1
+ (lambda(x) (+ 1 x))
+ 100)
+
+(product (lambda(x) x) 1 (lambda(x) (+ 1 x)) 15)
