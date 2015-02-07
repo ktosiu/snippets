7179a1987ba5d46ad3355cd982a58891d852c24d
zouivex <zouivex@gmail.com>
2015-02-08 00:13:42 +0800

    Introcution to algorithms

---
+++ algorithm/ITA/ch2/insertion_sort.py
@@ -0,0 +1,13 @@
+def insertion_sort(A):
+    n = len(A)
+    for i in range(1, n):
+        key = A[i]
+        j = i - 1
+        while j >= 0 and A[j] > key:
+            A[j+1] = A[j]
+            j = j - 1
+        A[j+1] = key
+
+unsorted_data = [3, 4, 1, 6, 9, 2, 7]
+insertion_sort(unsorted_data)
+print unsorted_data
