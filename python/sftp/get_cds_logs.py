9deff99ba68e06afd91271ba887f9d6f5ed52ce9
zouivex <zouivex@gmail.com>
2015-02-10 07:57:17 +0000

    Only get the latest file

--- python/sftp/get_cds_logs.py
+++ python/sftp/get_cds_logs.py
@@ -14,15 +14,23 @@ remote_hosts = {
     '10.117.171.18': ('root', 'Reuters1')
 }
 
+#remote_hosts = {
+#    '10.117.171.13': ('root', 'Reuters1'),
+#    '10.117.171.14': ('root', 'Reuters1')
+#}
+
 interested_paths = {
-    #'events': '/data/cds/events',
+    'events': '/data/cds/events'
     #'stats': '/data/cds/stats',
+    #'statsrecovery': '/data/cds/statsrecovery'
+    #'cachedump': '/data/cds/realtimecache'
     #'pcaps': '/data/cds/pcap'
-    'events': '/data/cds/events',
-    'pcaps': '/data/cds/pcap'
+    #'events': '/data/cds/events'
+    #'pcaps': '/data/cds/pcap'
+    #'smflogs': '/data/smf/log'
 }
 
-local_root_path = '/root/sit_logs'
+local_root_path = '/root/cds_events'
 
 def main():
     prepare_local_path()
@@ -49,10 +57,25 @@ def get_interested_files(hostname, name, passwd):
             local_path = local_root_path + '/' + hostname
             if not os.path.exists(local_path):
                 os.mkdir(local_path)
-            try:
-                sftp.get_d(path, local_path, preserve_mtime=True)
-            except:
-                print 'Fail to copy ' + path
+
+            with sftp.cd(path):
+                max_mtime = 0
+                file_name = ''
+                file_size = 0
+                for attr in sftp.listdir_attr():
+                    if attr.st_mtime > max_mtime:
+                        max_mtime = attr.st_mtime
+                        file_name = attr.filename
+                        file_size = attr.st_size
+                file_size = file_size / (1024.0 * 1024.0)
+                str_file_size = '%.2f' % file_size
+                log = 'Copy {0} ({1}M) to {2}'.format(file_name, str_file_size, local_path)
+                print log
+                try:
+                    os.chdir(local_path)
+                    sftp.get(file_name, preserve_mtime=True)
+                except:
+                    print 'Fail to copy ' + file_name
 
 if __name__ == '__main__':
     sys.exit(main())
