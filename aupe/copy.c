5812a30ac0c3810158caf629186140e976e0466e
zouivex <zouivex@gmail.com>
2015-03-03 23:16:49 +0800

    AUPE

---
+++ aupe/copy.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdio.h>
+
+#define BUFSIZE 4096
+
+int main() {
+    int n = 0;
+    char buf[BUFSIZE];
+    while ((n = read(STDIN_FILENO, buf, BUFSIZE)) > 0) {
+        if (write(STDOUT_FILENO, buf, n) != n) {
+            printf("write error\n");
+            return -1;
+        }
+    }
+
+    if (n < 0) {
+        printf("read error\n");
+    }
+
+    return 0;
+}
---
+++ aupe/ls.c
@@ -0,0 +1,24 @@
+/*opendir, readdir, closedir*/
+#include <dirent.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    DIR* dp;
+    struct dirent* dirp;
+
+    if (argc != 2) {
+        printf("usage: ls dir_name\n");
+        return -1;
+    }
+
+    if ((dp = opendir(argv[1])) == NULL) {
+        printf("can't open %s\n", argv[1]);
+    }
+
+    while ((dirp = readdir(dp)) != NULL) {
+        printf("%s\n", dirp->d_name);
+    }
+
+    closedir(dp);
+    return 0;
+}
