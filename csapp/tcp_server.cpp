31120556acfeb039f574ea4c1ae25571c7305c81
zouivex <zouivex@gmail.com>
2015-08-07 23:24:56 +0800

    tcp_server change

--- csapp/tcp_server.cpp
+++ csapp/tcp_server.cpp
@@ -1,44 +1,53 @@
-#include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
-#include <string.h>
-#include <unistd.h>
-#include <iostream>
+#include <arpa/inet.h>
 
-using namespace std;
+#include <stdio.h>
+#include <string.h> // memset
+#include <unistd.h> 
 
 typedef struct sockaddr SA;
 
-int open_listenfd(int port) {
-    int listenfd;
-    int option = 1;
+int open_listen_fd(int port) {
     struct sockaddr_in server_addr;
-
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-
-    //setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void*)&option, sizeof(int));
 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons((unsigned int)port);
+    server_addr.sin_port = htons(port);
 
-    bind(listenfd, (SA*)&server_addr, sizeof(server_addr));
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    listen(listenfd, 10);
+    if(listen_fd < 0) {
+        perror("socket:");
+        return -1;
+    }
 
-    return listenfd;
+    if (bind(listen_fd, (SA*)&server_addr, sizeof(struct sockaddr)) < 0) {
+        perror("bind:");
+        return -1;
+    }
+
+    //max queue for pending connections 5
+    listen(listen_fd, 5);
+
+    return listen_fd;
 }
 
-int main() {
-    int listenfd = open_listenfd(8000);
-    cout << "Listen fd: " << listenfd << endl;
+int main(int argc, char** argv) {
+    int listen_fd = open_listen_fd(8000);
+    if(listen_fd < 0) return -1;
+        
     struct sockaddr_in client_addr;
-    socklen_t client_len = sizeof(client_addr);
-    while(true) {
-        int connection_fd = accept(listenfd, (SA*)&client_addr, &client_len);
-        cout << "Connected" << endl;
-        close(connection_fd);
+    socklen_t len = sizeof(struct sockaddr_in);
+    int conn_fd = accept(listen_fd, (struct sockaddr*)&client_addr, &len);
+    if(conn_fd < 0) {
+        perror("accept:");
+        return -1;
     }
+
+    close(conn_fd);
+    close(listen_fd);
     return 0;
 }
